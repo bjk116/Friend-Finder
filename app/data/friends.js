d76ffd501c148f4d27cf614beeb92fe17de1d481
Brian K <karabinchak.brian@gmail.com>
2017-06-27 13:02:30 -0400

    Initial commit, basic html routes set up, import friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,24 @@
+//Friends data
+
+var friendsList = [
+		{
+			name: "Brian",
+			photo: "www.google.com",
+			scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+		}, {
+			name: "Haley",
+			photo: "www.yahoo.com",
+			scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+		}, {
+			name: "Kimmy",
+			photo: "www.bing.com",
+			scores: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+		}, {
+			name: "Titus",
+			photo: "www.facebook.com",
+			scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 3]
+		}
+	];
+
+
+module.exports.friendsList = friendsList;
---
+++ server.js
@@ -0,0 +1,59 @@
+//Main get/post javascript file
+
+//Dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var friends = require('./app/data/friends');
+
+//Handle to the friendsList
+var friendsList = friends.friendsList;
+
+// For Importing later on, figure this out later
+// var api = require('./apps/routing/apiRoutes.js');
+// var HTML = require('./apps/routing/htmlRoutes.js');
+
+console.log(friends.friendsList[0]);
+//Maybe if time permits, do this using database
+// var MySQL = require('mysql');
+
+//Handles
+var app = express();
+var PORT = 3000;
+
+//Express Middle-Ware
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+//To go in htmlRoutes.js
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "app/public/home.html"));
+});
+
+app.get("/survey", function(req, res) {
+  res.sendFile(path.join(__dirname, "app/public/survey.html"));
+});
+
+//To go in apiRoutes.js
+app.post("/api/new", function(req, res) {
+    var newReservation = req.body;
+    //If there are already 5 tables waiting
+    if (currentWaitingList.length >= 5) {
+        //push new reservation to extended waiting list
+        extendedWaitingList.push(newReservation);
+    } else {
+        currentWaitingList.push(newReservation);
+    }
+});
+
+app.get("/api/friends", function(req, res) {
+	res.send(friendsList);
+});
+
+//Start Server
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
